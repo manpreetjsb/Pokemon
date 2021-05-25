6f361f1e49c03d1f02fe41452874f5086a66eaea
manpreet singh <manpreet_jsb@hotmail.com>
2021-05-25 20:28:27 +0200

    added more tests

--- src/components/pokemonList/pokemonList.test.tsx
+++ src/components/pokemonList/pokemonList.test.tsx
@@ -3,6 +3,7 @@ import { render, fireEvent, waitFor } from '@testing-library/react'
 import fetch from 'node-fetch'
 import { mocked } from 'ts-jest/utils'
 import PokemonList from '.'
+import { DetailProp } from '../pokemonDetail/pokemonDetail.test'
 
 jest.mock('node-fetch', () => {
   return jest.fn()
@@ -10,19 +11,6 @@ jest.mock('node-fetch', () => {
 
 beforeEach(() => {
   mocked(fetch).mockClear()
-})
-
-mocked(fetch).mockImplementation((): Promise<any> => {
-  return Promise.resolve({
-    json() {
-      return Promise.resolve({
-        results: [
-          { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
-          { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
-        ],
-      })
-    },
-  })
 })
 
 const setup = () => {
@@ -34,19 +22,62 @@ const setup = () => {
 }
 
 describe('PokemonList', () => {
-  it('should has list', async () => {
+  it('should load list', async () => {
+    mocked(fetch).mockImplementation((): Promise<any> => {
+      return Promise.resolve({
+        json() {
+          return Promise.resolve({
+            results: [
+              {
+                name: 'charmander',
+                url: 'https://pokeapi.co/api/v2/pokemon/4/',
+              },
+              { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+            ],
+          })
+        },
+      })
+    })
     const { queryByText } = setup()
     await waitFor(() => {
-      expect(queryByText('bulbasaur')).toBeInTheDocument()
+      expect(queryByText('charmander')).toBeInTheDocument()
       expect(queryByText('ivysaur')).toBeInTheDocument()
     })
   })
 
-  /*   it('should click', async () => {
-    const { queryByText } = setup()
-    await waitFor(() => {
-      expect(queryByText('bulbasaur')).toBeInTheDocument()
-      expect(queryByText('ivysaur')).toBeInTheDocument()
+  it('should click load other component', async () => {
+    mocked(fetch).mockImplementation((): Promise<any> => {
+      return Promise.resolve({
+        json() {
+          return Promise.resolve({
+            results: [
+              {
+                name: 'charmander',
+                url: 'https://pokeapi.co/api/v2/pokemon/4/',
+              },
+              { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+            ],
+          })
+        },
+      })
     })
-  }) */
+
+    const { queryByText, getByTestId } = setup()
+    await waitFor(() => {
+      expect(queryByText('charmander')).toBeInTheDocument()
+      expect(queryByText('ivysaur')).toBeInTheDocument()
+      mocked(fetch).mockImplementation((): Promise<any> => {
+        return Promise.resolve({
+          json() {
+            return Promise.resolve({
+              ...DetailProp,
+            })
+          },
+        })
+      })
+      fireEvent.click(queryByText('charmander') as HTMLButtonElement)
+    })
+
+    expect(getByTestId('pokemon-name').textContent).toEqual('charmander')
+  })
 })
